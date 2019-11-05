92796c71a8005f48badff0daac741abeb3300aaf
SergeM <sbmatyunin@gmail.com>
2019-11-05 22:29:26 +0100

    working script running and status display

--- web_ui/index.js
+++ web_ui/index.js
@@ -1,20 +1,142 @@
-const express = require('express')
-const expressWS = require('express-ws') 
-const path = require('path')
-const app = express()
-const appWS = expressWS(app)
-const port = 3000
+const express = require('express');
+const expressWS = require('express-ws');
+const WebSocket = require('ws');
+const path = require('path');
+const app = express();
+const appWS = expressWS(app);
+const port = 3000;
+const { spawn } = require('child_process');
+
+const logCommandsOutput = false;
+const numCharsToSave = 100;
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+  console.log("root endpoint");
+  response.send('Hello from Express!');;
 })
 
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
-app.ws('/', (s, req) => {
-  console.error('websocket connection');
-  for (var t = 0; t < 3; t++)
-    setTimeout(() => s.send('message from server', ()=>{}), 1000*t);
+tracked_processes = {
+  "dummy_script": {
+    "process": null,
+    "output": "",
+    "update_function": null,
+    "command": 'python',
+    "args": ['dummy_script.py'],
+    "return_code": null
+  },
+  "dummy_script_failing": {
+    "process": null,
+    "output": "",
+    "update_function": null,
+    "command": 'python',
+    "args": ['dummy_script_failing.py'],
+    "return_code": null
+  }
+};
+
+var socket_clients = [];
+
+function prepare_status_update() {
+  all_updates = [];
+  for (var name in tracked_processes) {
+    var process_info = tracked_processes[name];
+    var data_to_send = {
+      "command": "update_status",
+      "name": name,
+      "status": "off",
+      "data": process_info.output,
+      "return_code": process_info.return_code
+    };
+    if (process_info.process) {
+      data_to_send.status = "running";
+    }
+    all_updates.push(JSON.stringify(data_to_send));
+  }
+  return all_updates;
+}
+
+function update_clients() {
+  all_updates = prepare_status_update();
+  socket_clients.forEach((ws) => {
+    if (ws.readyState !== WebSocket.OPEN) {
+      return;
+    }
+    all_updates.forEach(
+      (data_to_send) => { ws.send(data_to_send); }
+    );   
+  });
+}
+
+setInterval(update_clients, 1000);
+
+function process_start(name) {
+  var process_info = tracked_processes[name];
+    if (process_info.process) {
+      console.error("process " + name + " is already running");
+      return;
+    }
+    console.error("starting process " + name);
+    var p = spawn(process_info.command, process_info.args, { stdio: ['pipe'] });
+
+    p.stdout.on('data', function (data) {
+      if (logCommandsOutput) {
+        console.debug(name + ": " + data.toString().replace("\n", "\\n"));
+      }
+      process_info.output = (process_info.output + data.toString()).slice(-numCharsToSave);
+    });
+    p.on('close', (code) => {
+      console.log(`process ${process_info.command} ${process_info.args} exited with code ${code}`);
+      process_info.return_code = code;
+      process_info.process = null;
+    });
+
+    process_info.process = p;
+    process_info.return_code = null;
+}
+
+function process_stop(name) {
+  var process_info = tracked_processes[name];
+    if (!process_info.process) {
+      console.error("process " + name + " is not running");
+      return;
+    }
+    console.error("stopping process " + name);
+    process_info.process.kill();
+    process_info.process = null;
+}
+
+function process_message(msg_json) {
+  console.log("Processing ", msg_json);
+  if (msg_json.command == "start") {
+    return process_start(msg_json.name);
+  }
+  if (msg_json.command == "stop") {
+    return process_stop(msg_json.name);
+  }
+}
+
+app.ws('/', (ws, req) => {
+  
+  console.error('connected..');
+
+  ws.on('message', function (msg) {
+    console.log(msg);
+    msg_json = JSON.parse(msg);
+    process_message(msg_json);
+  });
+
+  ws.on('error', function (err) {
+    console.log('Found error: ' + err);
+  });
+
+  ws.on('close', function () {
+    console.log('connection closed.');
+  });
+
+  socket_clients.push(ws);
+  console.info("num clients: ", socket_clients.length);
 });
 
 
@@ -22,7 +144,15 @@ app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
-
   console.log(`server is listening on ${port}`)
 })
 
+
+process.on('SIGINT', function () {
+  console.log("terminating...")
+  for (var name in tracked_processes) {
+    process_stop(name);
+  }
+  update_clients();
+  process.exit();
+});
--- web_ui/static/js/my_controller.js
+++ web_ui/static/js/my_controller.js
@@ -1,7 +1,66 @@
-angular.module('myApp', []).controller('personCtrl', function($scope) {
-    $scope.firstName = "John",
-    $scope.lastName = "Doe",
-    $scope.fullName = function() {
+angular.module('myApp', []).controller('launcherCtrl', function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function () {
         return $scope.firstName + " " + $scope.lastName;
-    }
+    };
+
+    $scope.scriptStatus = "unknown"
+    $scope.nodeNames = ["dummy_script", "dummy_script_failing"]
+    $scope.nodeStatuses = $scope.nodeNames.map((x)=> "");
+
+    $scope.connectionStatus = "";
+
+    $scope.getSocketUrl = function () {
+        return 'ws://' + window.location.host + '/';
+    };
+
+    var wsUri = $scope.getSocketUrl()
+    var websocket = new WebSocket(wsUri);
+
+    websocket.onopen = function (evt) {
+        console.log("connection established to ", wsUri);
+
+        $scope.$apply(function () {
+            $scope.connectionStatus = $scope.connectionStatus + "connected to " + wsUri;
+        });
+    };
+
+    websocket.onerror = function (evt) {
+        console.error('websocket error: ' + evt);
+        
+    };
+
+    websocket.onclose = function () {
+        $scope.connectionStatus = "closed";
+    };
+
+    websocket.onmessage = function (evt) {
+        var msg_json = JSON.parse(evt.data);
+
+        $scope.$apply(function () {
+            if (msg_json.command == "update_status") {
+                $scope.nodeStatuses[msg_json.name] = 
+                    "status: " + msg_json.status + 
+                    ", return_code: " + msg_json.return_code + 
+                    ", output: " + msg_json.data;
+            }
+        });
+    };
+
+    $scope.startScript = function (nodeName) {
+        $scope.scriptStatus = $scope.scriptStatus + ", starting...";
+        websocket.send(JSON.stringify({
+            "command": "start",
+            "name": nodeName
+        }));
+    };
+
+    $scope.stopScript = function (nodeName) {
+        $scope.scriptStatus = $scope.scriptStatus + ", stopping...";
+        websocket.send(JSON.stringify({
+            "command": "stop",
+            "name": nodeName
+        }));
+    };
 });
