e4483648844d82d720727dfefb21f2ab1364779d
SergeM <sbmatyunin@gmail.com>
2019-11-05 18:54:07 +0100

    added draft for angularjs

--- web_ui/index.js
+++ web_ui/index.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const expressWS = require('express-ws') 
 const path = require('path')
 const app = express()
-const port = 9555
+const appWS = expressWS(app)
+const port = 3000
 
 app.get('/', (request, response) => {
   response.send('Hello from Express!')
 })
 
 app.use('/static', express.static(path.join(__dirname, 'static')))
+
+app.ws('/', (s, req) => {
+  console.error('websocket connection');
+  for (var t = 0; t < 3; t++)
+    setTimeout(() => s.send('message from server', ()=>{}), 1000*t);
+});
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -16,3 +25,4 @@ app.listen(port, (err) => {
 
   console.log(`server is listening on ${port}`)
 })
+
